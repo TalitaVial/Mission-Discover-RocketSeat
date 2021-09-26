23173d0bda4f9fdb5d04942aa98ae790d4668b57
Talita Vial <talitavialsilva1@gmail.com>
2021-09-26 16:54:50 -0400

    Aula 4 RocketSeat Mission Discover

--- src/route.js
+++ src/route.js
@@ -1,11 +1,16 @@
 const express = require('express')
+const QuestionController = require('./controllers/QuestionController')
 
 const route = express.Router()
 
-route.get('/', (req, res) => res.render('index'))
+route.get('/', (req, res) => res.render('index', { page: 'enter-room' }))
+route.get('/create-pass', (req, res) =>
+  res.render('index', { page: 'create-pass' })
+)
 route.get('/room', (req, res) => res.render('room'))
-route.get('/create-pass', (req, res) => res.render('create-pass'))
 
-//route.get('/room/:room/:question/:action') formato do informação que deve ser passada pelo formulário
+//formato do informação que deve ser passada pelo formulário
+
+route.post('/room/:room/:question/:action', QuestionController.index)
 
 module.exports = route
