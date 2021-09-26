2d61949d2adfb1d725e431ea24b78fd71f1b60e8
Talita Vial <talitavialsilva1@gmail.com>
2021-09-26 11:25:48 -0400

    Aula 4 RocketSeat Mission Discover

--- src/route.js
+++ src/route.js
@@ -6,4 +6,6 @@ route.get('/', (req, res) => res.render('index'))
 route.get('/room', (req, res) => res.render('room'))
 route.get('/create-pass', (req, res) => res.render('create-pass'))
 
+//route.get('/room/:room/:question/:action') formato do informação que deve ser passada pelo formulário
+
 module.exports = route
