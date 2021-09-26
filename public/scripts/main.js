01c8c2752a2f2497f755f43f5452de7a0070fe32
Talita Vial <talitavialsilva1@gmail.com>
2021-09-25 22:03:54 -0400

    Aula 4 RocketSeat Mission Discover

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,10 +1,22 @@
-import { Modal } from './Modal'
+import Modal from './modal.js'
 
 const modal = Modal()
 //pegar todos os botões com a classe check
 const checkButtons = document.querySelectorAll('.actions a.check')
 
 //pegar o click no marcar como lido
+checkButtons.forEach(button => {
+  //adicionar a escuta de eventos
+  button.addEventListener('click', event => {
+    //abrir a modal
+    modal.open()
+  })
+})
 
-//abrir a modal
-modal.open()
+const deleteButton = document.querySelectorAll('.actions .delete')
+
+deleteButton.forEach(button => {
+  button.addEventListener('click', event => {
+    modal.open()
+  })
+})
--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,10 +1,17 @@
-module.exports = function Modal() {
+export default function Modal() {
+  const modalWrapper = document.querySelector('.modal-wrapper')
+  const cancelButton = document.querySelector('.button.cancel')
+
+  cancelButton.addEventListener('click', close)
+
   function open() {
     //funcionalidade de atribuir a classe active na modal
+    modalWrapper.classList.add('active')
   }
 
   function close() {
     //remover a classe active do modal
+    modalWrapper.classList.remove('active')
   }
 
   return {
