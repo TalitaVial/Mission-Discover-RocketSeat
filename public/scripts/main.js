006cf603eca0846cf108a0f2f09d9320f3dc32dc
Talita Vial <talitavialsilva1@gmail.com>
2021-09-25 21:34:34 -0400

    Aula 4 RocketSeat Mission Discover

---
+++ public/scripts/main.js
@@ -0,0 +1,10 @@
+import { Modal } from './Modal'
+
+const modal = Modal()
+//pegar todos os botões com a classe check
+const checkButtons = document.querySelectorAll('.actions a.check')
+
+//pegar o click no marcar como lido
+
+//abrir a modal
+modal.open()
---
+++ public/scripts/modal.js
@@ -0,0 +1,14 @@
+module.exports = function Modal() {
+  function open() {
+    //funcionalidade de atribuir a classe active na modal
+  }
+
+  function close() {
+    //remover a classe active do modal
+  }
+
+  return {
+    open,
+    close
+  }
+}
